42bf2bf43a3013b45ea6f1e73e72f2568b078585
Michał Szatkowski <michal.szatkowski@vp.pl>
2023-04-26 10:34:01 +0200

    i

---
+++ ExecSo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("/home/xxx/invoke/hi.so")]
+    private static extern int Sayhi();
+
+    static void Main(string[] args)
+    {
+     
+        Console.WriteLine("Hello World!");
+        int status = Sayhi();
+        Console.WriteLine(status);
+
+    }
+}
---
+++ SoC/Source.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int Sayhi() {
+	cout << "hello world from cpp!" << endl;
+	return 1;
+}
